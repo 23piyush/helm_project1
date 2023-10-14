620c31172581119b8522817728728958f16a328f
23piyush <48410575+23piyush@users.noreply.github.com>
2023-10-14 12:54:31 +0530

    Update script.sh

--- practice1/script.sh
+++ practice1/script.sh
@@ -1,7 +1,7 @@
 # Move to correct repo
 cd practice1
 
-# Install helm
+# Install helm using script
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
